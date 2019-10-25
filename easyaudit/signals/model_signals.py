22e4d281d35bd741271abdb1a606026928156a94
Gaurav <gaurav.ghildyal@itoworld.com>
2019-10-25 16:24:20 +0100

    Merge 0f44bb606b0001f247d966ab2fb57856bcdc0dc2 - dont update when no fields have changed

--- easyaudit/signals/model_signals.py
+++ easyaudit/signals/model_signals.py
@@ -67,7 +67,9 @@ def pre_save(sender, instance, raw, using, update_fields, **kwargs):
             if not created:
                 old_model = sender.objects.get(pk=instance.pk)
                 delta = model_delta(old_model, instance)
-                changed_fields = json.dumps(delta) if delta else None
+                if not delta:
+                    return False
+                changed_fields = json.dumps(delta)
                 event_type = CRUDEvent.UPDATE
 
             # user
@@ -83,8 +85,6 @@ def pre_save(sender, instance, raw, using, update_fields, **kwargs):
 
             # callbacks
             kwargs['request'] = get_current_request()  # make request available for callbacks
-            if not created:
-                kwargs['changed_fields'] = changed_fields
             create_crud_event = all(
                 callback(instance, object_json_repr, created, raw, using, update_fields, **kwargs)
                 for callback in CRUD_DIFFERENCE_CALLBACKS if callable(callback))
